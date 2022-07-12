1c212b3ff616aac9bb50b237d55ca70566197037
marygladysbandela <mary.bandela@gmail.com>
2022-07-12 10:38:56 +0530

    some comment why we changed code

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package first_java_project;
+
+/**
+ * @author Noel
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome");
+
+	}
+
+}
